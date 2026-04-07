28e4b2740761d22527af10167ccf82a760c468ae
Shalini Agarwal <shaliniag12@gmail.com>
2026-04-06 19:53:39 -0500

    Scraped job location and department along with title and link. Also, formed the complete job link.

--- scrape.py
+++ scrape.py
@@ -1,8 +1,7 @@
 '''
 Things to do next:
-    1. Capture the location and department along with title and job link.
-    2. Figure out how to scrape all the data and not get affected by pagination.
-    3. Form the complete job link URL by pre-prending stripe.com.
+    1. Figure out how to scrape all the data and not get affected by pagination.
+    2. Filter out software engineering roles only.
 '''
 
 
@@ -18,19 +17,41 @@ soup = BeautifulSoup(source, 'lxml')
 csv_file = open('stripe-jobs-scrape.csv', 'w')
 
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['job_title', 'job_link'])
+csv_writer.writerow(['title', 'team', 'location', 'link'])
 
-# all_jobs = soup.find('tbody', class_='TableBody JobsListings__tableBody')
+all_jobs = soup.find('tbody', class_='TableBody JobsListings__tableBody')
 
-# one_job = all_jobs.find('tr', class_="TableRow")
 
-# print(one_job)
+## For a single job post
 
-for job_listing in soup.find_all('a', class_='JobsListings__link'):
-    job_title = job_listing.text
-    job_link = job_listing['href']
+# job_listing = all_jobs.find('tr', class_="TableRow")
 
-    csv_writer.writerow([job_title, job_link])
+# job_info = job_listing.find('a', class_='JobsListings__link')
+# job_title = job_info.text
+# job_link = job_info['href']
+# complete_link = f"https://stripe.com{job_link}"
+
+# job_team = job_listing.find('li', class_ = 'List__item ListItem JobsListings__departmentsListItem').text.strip()
+# job_location = job_listing.find('span', class_ = 'JobsListings__locationDisplayName').text
+
+# print(job_title)
+# print(job_team)
+# print(job_location) 
+# print(complete_link)
+
+
+
+for job_listing in all_jobs.find_all('tr', class_="TableRow"):
+
+    job_info = job_listing.find('a', class_='JobsListings__link')
+    job_title = job_info.text
+    job_link = job_info['href']
+    complete_link = f"https://stripe.com{job_link}"
+
+    job_team = job_listing.find('li', class_ = 'List__item ListItem JobsListings__departmentsListItem').text.strip()
+    job_location = job_listing.find('span', class_ = 'JobsListings__locationDisplayName').text
+
+    csv_writer.writerow([job_title, job_team, job_location, complete_link])
 
 
 
